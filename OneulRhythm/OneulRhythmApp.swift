73d63afa0206ff97e058618fe89a004327925b5f
yiu0905 <kodog102@gmail.com>
2026-07-21 17:04:12 +0900

    chore: remove unused SwiftData template model

--- OneulRhythm/OneulRhythmApp.swift
+++ OneulRhythm/OneulRhythmApp.swift
@@ -12,7 +12,6 @@ import SwiftData
 struct OneulRhythmApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
             RoutineEntity.self,
             RecurringRhythmEntity.self,
         ])
